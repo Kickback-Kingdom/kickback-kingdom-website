12530fe90c60e3e5c79a7f1c1a4784204fc8a87a
Chad Joan <chadjoan@gmail.com>
2024-02-10 04:59:13 -0500

    Remove obsolete `html/components` directory.

--- html/components/breadcrumb.php
+++
@@ -1,8 +0,0 @@
-
-<div class="col-12 col-sm-6">
-    <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="index.html"><i data-feather="home"></i></a></li>
-        <li class="breadcrumb-item">Page layout</li>
-        <li class="breadcrumb-item active">Boxed</li>
-    </ol>
-</div>
