a4adcfaad7d891ca1f468a996266b987c2f24792
Chad Joan <chadjoan@gmail.com>
2024-04-01 16:30:19 -0400

    Include autoloader from correct script root.

--- html/Kickback/init.php
+++ html/Kickback/init.php
@@ -1,6 +1,32 @@
 <?php
 
+// An important special (and tricky) property of this file:
+//
+// It may be running from the `prod` SCRIPT_ROOT while the site is being
+// accessed in `beta` mode. Normally, if the site is in `beta` mode,
+// we would be in a different SCRIPT_ROOT for `beta`.
+//
+// It might be running from `prod` while in `beta` mode because the scripts
+// including (`require`'ing) this file do not have \Kickback\SCRIPT_ROOT
+// defined yet, and must rely on using something like `$_SERVER['DOCUMENT_ROOT']`
+// to locate the `init.php` file. And it's that use of `$_SERVER['DOCUMENT_ROOT']`
+// that can place us at the incorrect script root.
+//
+// One important implication of the above is that we should not `require_once`
+// on relative paths from this file, except for `script_root.php` itself.
+// We need to use a relative path for `script_root.php` because it's what
+// computes+defines \Kickback\SCRIPT_ROOT, and we have no better options
+// for locating scripts until that is done. AFTER that, we should reference
+// scripts using ONLY \Kickback\SCRIPT_ROOT (while in this file),
+// thus restoring inclusion to the correct script root.
+
+// File that declares \Kickback\SCRIPT_ROOT.
+// Do this first, so that we can use it to locate the correct script files.
 require_once("script_root.php");
-require_once("autoload_classes.php");
+
+// Initialize+register the autoloader for \Kickback namespace classes,
+// and for any classes that are manually managed with the project
+// (ex: things in (\Kickback\SCRIPT_ROOT . "/vendor"))
+require_once(\Kickback\SCRIPT_ROOT . "/Kickback/autoload_classes.php");
 
 ?>
