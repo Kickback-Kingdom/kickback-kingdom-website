6e05b7ec0495f21dfa4251a62faa5e6e88938b02
AlibabaTheGreat <159730217+AlibabaTheGreat@users.noreply.github.com>
2024-12-07 23:06:02 -0300

    0-1-2 update

--- html/Kickback/Common/Version.php
+++ html/Kickback/Common/Version.php
@@ -32,7 +32,8 @@ final class Version
     private const HISTORY = [
         [0,0,1,"","",   "update-writ-of-passage"],
         [0,1,0,"","", "backend-overhaul"],
-        [0,1,1,"","", "elo-ranking-update"]
+        [0,1,1,"","", "elo-ranking-update"],
+        [0,1,2,"","", "ranked-match-history"]
     ];
 
     //private const LAST_HISTORY_ENTRY_IDX = array_key_last(self::HISTORY);
