ca8822379536da2b9137d21cd346fd690de969fb
Chad Joan <chadjoan@gmail.com>
2025-07-02 02:06:59 -0400

    Add two new exception types:
    ImmutableModificationException and UnsupportedOperationException.
    
    As of this commit, they are unused. But they are used in unmerged code,
    so they will come into use as WIP/future features become implemented and
    merged in.

---
+++ html/Kickback/Common/Exceptions/UnsupportedOperationException.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types=1);
+namespace Kickback\Common\Exceptions;
+
+use Kickback\Common\Exceptions\CustomException;
+
+class UnsupportedOperationException extends CustomException {}
+?>
