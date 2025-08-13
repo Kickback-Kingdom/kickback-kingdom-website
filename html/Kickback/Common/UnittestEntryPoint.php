9fd7df73da492724eb9687ca6a4198291a267cb0
Chad Joan <chadjoan@gmail.com>
2025-08-13 16:37:38 -0400

    Add PHP_BinaryOps class.
    This provides a list of constant integers that represent all of the
    binary operators that can appear in PHP code. It can also dispatch these
    IDs to execute the described operation.

--- html/Kickback/Common/UnittestEntryPoint.php
+++ html/Kickback/Common/UnittestEntryPoint.php
@@ -24,6 +24,7 @@ class UnittestEntryPoint
         \Kickback\Common\Primitives\Meta::unittests();
         \Kickback\Common\Exceptions\ThrowableContextMessageHandling::unittests();
         \Kickback\Common\Meta\ZType::unittests(); // Depends on Common\Primitives\Meta.
+        \Kickback\Common\Meta\PHP_BinaryOps::unittests(); // Depends on Common\Primitives\Meta.
         \Kickback\Common\Primitives\Arr::unittests(); // May depend on Common\Primitives\Meta in the future?
         \Kickback\Common\Primitives\Mixed_::unittests(); // Depends on Common\Primitives\Meta.
         \Kickback\Common\Primitives\Str::unittests(); // May depend on Common\Primitives\Meta in the future?
